b9da231f53f19829c6eb710eee1e0e196671bdf7
denisenk00 <dmitriy.denisenko.2002@gmail.com>
2021-05-16 18:06:26 +0300

    Initial commit

---
+++ src/main/java/ua/edu/j2ee/shoestore/ShoestoreApplication.java
@@ -0,0 +1,21 @@
+package ua.edu.j2ee.shoestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"ua.edu.j2ee.shoestore.dao",
+        "ua.edu.j2ee.shoestore.model",
+        "ua.edu.j2ee.shoestore.rest",
+        "ua.edu.j2ee.shoestore.services",
+        "ua.edu.j2ee.shoestore.config",
+        "ua.edu.j2ee.shoestore.controllers",
+        "ua.edu.j2ee.shoestore.security"})
+public class ShoestoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoestoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ua/edu/j2ee/shoestore/ShoestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.edu.j2ee.shoestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoestoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
