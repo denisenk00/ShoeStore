52961131fc66e16e7c5c2b328c4ebf761d6b5a69
NNc31 <Nefyodov.Nazar@outlook.com>
2021-05-17 16:04:41 +0300

    add UserService and role

---
+++ src/main/java/ua/edu/j2ee/shoestore/services/UserService.java
@@ -0,0 +1,13 @@
+package ua.edu.j2ee.shoestore.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ua.edu.j2ee.shoestore.dao.UserDao;
+import ua.edu.j2ee.shoestore.model.User;
+
+@Service
+public interface UserService {
+
+    void registerAccount(User user);
+
+}
---
+++ src/main/java/ua/edu/j2ee/shoestore/services/UserServiceImpl.java
@@ -0,0 +1,24 @@
+package ua.edu.j2ee.shoestore.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import ua.edu.j2ee.shoestore.dao.UserDao;
+import ua.edu.j2ee.shoestore.model.User;
+
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserDao userDao;
+
+    @Override
+    public void registerAccount(User user) {
+        if (emailExists(user.getEmail())) {
+            throw new IllegalArgumentException("User with email " + user.getEmail() + " already exists");
+        }
+
+        userDao.save(user);
+    }
+
+    private boolean emailExists(String email) {
+        return userDao.getByEmail(email) != null;
+    }
+}
