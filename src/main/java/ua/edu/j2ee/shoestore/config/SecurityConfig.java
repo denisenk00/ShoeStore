8291abbb6674ed0e0153a0220bb9ba10e5144749
NNc31 <Nefyodov.Nazar@outlook.com>
2021-05-18 01:18:11 +0300

    add default login page

--- src/main/java/ua/edu/j2ee/shoestore/config/SecurityConfig.java
+++ src/main/java/ua/edu/j2ee/shoestore/config/SecurityConfig.java
@@ -3,24 +3,38 @@ package ua.edu.j2ee.shoestore.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import ua.edu.j2ee.shoestore.services.UserDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final UserDetailsService userDetailsService;
-
     @Autowired
-    public SecurityConfig(UserDetailsService userDetailsService) {
-        this.userDetailsService = userDetailsService;
-    }
+    private UserDetailsServiceImpl userDetailsService;
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf()
+                    .disable()
+                .authorizeRequests()
+                    .antMatchers("/registration").not().fullyAuthenticated()
+                    .antMatchers("/admin/**").hasRole("ADMIN")
+                    .antMatchers("/", "/store/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                    .formLogin()
+                .and()
+                    .logout();
+    }
+
+    @Autowired
+    protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService);
     }
 }
--- src/main/java/ua/edu/j2ee/shoestore/services/UserDetailsServiceImpl.java
+++ src/main/java/ua/edu/j2ee/shoestore/services/UserDetailsServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import ua.edu.j2ee.shoestore.dao.UserDao;
 import ua.edu.j2ee.shoestore.model.User;
