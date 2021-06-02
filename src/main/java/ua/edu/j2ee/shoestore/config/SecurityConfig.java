be72e221823be9d4d565138d1eb3c63cd08f218c
NNc31 <Nefyodov.Nazar@outlook.com>
2021-06-02 19:36:33 +0300

    add BCryptEncoder for passwords

--- src/main/java/ua/edu/j2ee/shoestore/config/SecurityConfig.java
+++ src/main/java/ua/edu/j2ee/shoestore/config/SecurityConfig.java
@@ -1,12 +1,16 @@
 package ua.edu.j2ee.shoestore.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import ua.edu.j2ee.shoestore.services.UserDetailsServiceImpl;
 
 @Configuration
@@ -16,6 +20,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UserDetailsServiceImpl userDetailsService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
+    }
 
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
@@ -35,6 +44,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
+        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
     }
 }
