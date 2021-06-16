b3d8b3763b486539d1f46b2b9af2e73875391f1d
denisenk00 <dmitriy.denisenko.2002@gmail.com>
2021-06-16 14:59:28 +0300

    refactored all controllers

--- src/main/java/ua/edu/j2ee/shoestore/config/AppContextConfig.java
+++ src/main/java/ua/edu/j2ee/shoestore/config/AppContextConfig.java
@@ -7,11 +7,13 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @PropertySource("classpath:application.properties")
 @ComponentScan(basePackages = {"ua.edu.j2ee.shoestore.*"})
+@EnableWebMvc
 public class AppContextConfig {
 
     @Autowired
---
+++ src/main/java/ua/edu/j2ee/shoestore/controllers/ServiceController.java
@@ -0,0 +1,34 @@
+package ua.edu.j2ee.shoestore.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import ua.edu.j2ee.shoestore.services.UserService;
+
+@Controller
+@RequestMapping("/service")
+public class ServiceController {
+
+    private UserService userService;
+
+    @Autowired
+    public ServiceController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/checkPhoneForExistence")
+    @ResponseBody
+    public ResponseEntity checkPhoneForExistence(@RequestParam(name="phone") String phone){
+        return ResponseEntity.ok().body(userService.phoneExists(phone));
+    }
+
+    @GetMapping("/checkEmailForExistence")
+    @ResponseBody
+    public ResponseEntity checkEmailForExistence(@RequestParam(name="email") String email){
+        return ResponseEntity.ok().body(userService.emailExists(email));
+    }
+}
